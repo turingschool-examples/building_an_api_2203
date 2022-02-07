fa30c91fe8c20ba4283c5effd69347bc25c7d38c
Jamison Ordway <jamisonordway@gmail.com>
2022-02-07 12:00:14 -0700

    Completes building an API lesson

---
+++ spec/requests/api/v1/books_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'The books API' do
+  it 'sends a list of books' do
+    create_list(:book, 3)
+
+    get '/api/v1/books'
+
+    books = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(books.count).to eq(3)
+
+    books.each do |book|
+      expect(book).to have_key(:id)
+      expect(book[:id]).to be_an(Integer)
+
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+
+      expect(book).to have_key(:author)
+      expect(book[:author]).to be_a(String)
+
+      expect(book).to have_key(:genre)
+      expect(book[:genre]).to be_a(String)
+
+      expect(book).to have_key(:summary)
+      expect(book[:summary]).to be_a(String)
+
+      expect(book).to have_key(:number_sold)
+      expect(book[:number_sold]).to be_an(Integer)
+    end
+  end
+end

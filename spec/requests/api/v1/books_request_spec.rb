7b9fd850c05eb050986ec88b35c68484c193ba13
Jamison Ordway <jamisonordway@gmail.com>
2022-07-12 10:03:12 -0600

    Creates hand-rolled BooksSerializer, updates request spec

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'The books API' do
 
     get '/api/v1/books'
 
-    books = JSON.parse(response.body, symbolize_names: true)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    books = response_body[:data]
 
     expect(response).to be_successful
     expect(books.count).to eq(3)
@@ -15,20 +17,18 @@ RSpec.describe 'The books API' do
       expect(book).to have_key(:id)
       expect(book[:id]).to be_an(Integer)
 
-      expect(book).to have_key(:title)
-      expect(book[:title]).to be_a(String)
+      expect(book).to have_key(:attributes)
+      expect(book[:attributes][:title]).to be_a(String)
 
-      expect(book).to have_key(:author)
-      expect(book[:author]).to be_a(String)
+      expect(book[:attributes][:author]).to be_a(String)
 
-      expect(book).to have_key(:genre)
-      expect(book[:genre]).to be_a(String)
+      expect(book[:attributes][:genre]).to be_a(String)
 
-      expect(book).to have_key(:summary)
-      expect(book[:summary]).to be_a(String)
+      expect(book[:attributes][:summary]).to be_a(String)
 
-      expect(book).to have_key(:number_sold)
-      expect(book[:number_sold]).to be_an(Integer)
+      expect(book[:attributes][:number_sold]).to be_an(Integer)
+
+      expect(book[:attributes]).to_not have_key(:created_at)
     end
   end
 end

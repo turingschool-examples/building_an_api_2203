b786f9c90dc8fee8d021b13b769aaeaec4dc856f
Jamison Ordway <jamisonordway@gmail.com>
2022-07-12 06:35:48 -0600

    Sets up Customizing JSON lesson

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :store_books
+  has_many :stores, through: :store_books
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index]
+      resources :books
+      resources :stores, only: [:index, :show]
     end
   end
 end
